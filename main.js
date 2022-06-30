157ef8d7e5912d9c05a2eb000d676fe60cd32a14
dd0sxx <contacttheotelonis@gmail.com>
2022-06-30 11:06:45 -0700

    added sqaures to contract calls

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const gasData = require('./gasData')
 
-function calculateGasToSquares (gas, name) {
+function calculateGasToSquares (gas, name, color) {
     // one square = 2000 units of gas
     let numOfSqaures = Math.floor(gas / 2000)
     let remainder = gas % 2000
-    addSquaresToDOM(name, numOfSqaures, remainder)
+    addSquaresToDOM(name, numOfSqaures, remainder, color)
 }
 
-function addSquaresToDOM (elementID, squares, remainder) {
+function addSquaresToDOM (elementID, squares, remainder, color) {
     const element = document.getElementById(elementID)
     let squareBox = document.createElement("div")
     squareBox.style.display = 'flex'
@@ -18,7 +18,7 @@ function addSquaresToDOM (elementID, squares, remainder) {
     // squareBox.style.flexDirection = 'row-reverse'
     squareBox.style.marginRight = '5px'
     for (let i = 0; i < squares; i++) {
-        let newSquare = square(10);
+        let newSquare = square(10, color);
         squareBox.appendChild(newSquare)
     } 
     // scoped for legibility, create the remainder square
@@ -26,24 +26,27 @@ function addSquaresToDOM (elementID, squares, remainder) {
         const division = (remainder / 20)
         const remainderWidth = (division * 10) / 100
         if (remainderWidth > 0) {
-            const remainderSquare = remainderWidth < 1 ? square(1) : square(remainderWidth)
+            const remainderSquare = remainderWidth < 1 ? square(1, color) : square(remainderWidth, color)
             squareBox.appendChild(remainderSquare)
         }
     }
     element.prepend(squareBox)
 }
 
-function square (width) {
+function square (width, color) {
     let square = document.createElement("div")
     square.style.width = `${width}px`
     square.style.height = '10px'
     square.style.margin = '1px'
-    square.style.backgroundColor = '#0091e6'
+    square.style.backgroundColor = color
     square.style.display = "block"
     return square;
 }
 
 
 gasData.opcodes.forEach(item => {
-    calculateGasToSquares(item.gas, item.name)
+    calculateGasToSquares(item.gas, item.name, '#0091e6')
+})
+gasData.contractCalls.forEach(item => {
+    calculateGasToSquares(item.gas, item.name, '#000')
 })

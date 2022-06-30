f32f50a9cf853b86fe0f15525ae7d4fd1314213e
dd0sxx <contacttheotelonis@gmail.com>
2022-06-30 10:18:00 -0700

    boxes are displaying and updating css

--- main.js
+++ main.js
@@ -8,9 +8,30 @@ function calculateGasToSquares (gas, name) {
 }
 
 function addSquaresToDOM (elementID, squares, remainder) {
-    console.log(elementID)
     const element = document.getElementById(elementID)
-    console.log(element)
+    let squareBox = document.createElement("div")
+    squareBox.style.display = 'flex'
+    squareBox.style.alignItems = 'center'
+    squareBox.style.flexWrap = 'wrap'
+    squareBox.style.maxWidth = '100px'
+    // squareBox.style.flexDirection = 'row-reverse'
+    squareBox.style.marginRight = '5px'
+    for (let i = 0; i < squares; i++) {
+        let newSquare = square(i * 10, 0);
+        console.log('new square: ', newSquare)
+        squareBox.appendChild(newSquare)
+    } 
+    element.prepend(squareBox)
+}
+
+function square (x, y) {
+    let square = document.createElement("div")
+    square.style.width = '10px'
+    square.style.height = '10px'
+    square.style.border = '1px solid #FFFFFF'
+    square.style.backgroundColor = '#FF0000'
+    square.style.display = "block"
+    return square;
 }
 
 gasData.opcodes.forEach(item => {
